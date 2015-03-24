8cd83966a5e64d71148bb3bea6176ddcef048d7e
SP <SP@SP-PC>
2015-03-24 15:01:33 +0900

    Version 1.3 * 초과 시 요금 부과 수정

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -2,7 +2,7 @@
  * 요금 계산 프로그램
  * 2015.3.23
  * @author 김우범
- * @version 1.2
+ * @version 1.3
  */
 
 
@@ -12,7 +12,7 @@ public class Condition {
 
 	public static void main(String[] args) {
 		
-		int dist = 25;
+		int dist = 20;
 		int total_fee = 0;
 		String car_type = "고속버스";
 		
@@ -20,18 +20,21 @@ public class Condition {
 		switch(car_type) {
 		case "고속버스":
 			total_fee += 850;
-			if(dist > 10) total_fee += (dist/10) * 300;
+			total_fee += (dist/10) * 300;
+			if(dist % 10 == 0) total_fee -= 300;
 			break;
 			
 		case "경차":
 			total_fee += 300;
-			if(dist > 10) total_fee += (dist/10) * 200;
+			total_fee += (dist/10) * 200;
+			if(dist % 10 == 0) total_fee -= 200;
 			break;
 			
 		default:
 			car_type = "그 외";
 			total_fee += 600;
-			if(dist > 10) total_fee += (dist/10) * 200;
+			total_fee += (dist/10) * 200;
+			if(dist % 10 == 0) total_fee -= 200;
 			break;
 		}
 		
