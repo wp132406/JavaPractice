bd070fbd82c4faa4851128483420130e3b76a230
SP <SP@SP-PC>
2015-04-14 15:41:50 +0900

    --

--- JavaPractice/src/org/dimigo/oop/Car3Test.java
+++ JavaPractice/src/org/dimigo/oop/Car3Test.java
@@ -6,23 +6,23 @@ package org.dimigo.oop;
 /**
  * <pre>
  * org.dimigo.oop
- *   └CarTest2
+ *   └Car2Test
  *
  * 1. 개요 : 
  * 2. 작성일 : 2015. 4. 14.
  * </pre>
  *
  * @author 		: 김우범
- * @version 	: 1.1
+ * @version 	: 1.0
  */
-public class CarTest2 {
-public static void main(String[] args) {
+public class Car3Test {
+	public static void main(String[] args) {
 		
 		// Declaration
-		
-		Car2 car1 = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
-		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
-		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+	
+		Car3 car1 = new Car3("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car3 car2 = new Car3("기아자동차", "K7", "흰색", 246);
+		Car3 car3 = new Car3("삼성자동차", "SM7", "회색");
 		
 		// Trash can
 		
