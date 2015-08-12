1fb61ae619697687da9155c2e25685beafb79ee6
SP <SP@SP-PC>
2015-08-12 09:51:22 +0900

    도형

---
+++ JavaPractice/src/org/dimigo/inheritance/Triangle.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   └Triangle
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 12.
+ * </pre>
+ *
+ * @author 		: 김우범
+ * @version 	: 1.0
+ */
+public class Triangle extends Figure {
+	
+	private int width;
+	private int height;
+	
+	public Triangle(int width, int height)
+	{
+		super(0, 0);
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Triangle(int centerX, int centerY, int width, int height)
+	{
+		super(centerX, centerY);
+		this.width = width;
+		this.height = height;
+	}
+	
+	protected double calcArea()
+	{
+		return width * height * 0.5;
+	}
+
+
+}
